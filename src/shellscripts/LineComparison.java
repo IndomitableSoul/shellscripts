ac1cf8eef9feb619ebb5b087266e8ed54025929c
nikita <amarnikita09@gmail.com>
2021-12-08 13:38:58 +0530

    committing LineComparison UC1 again

--- src/shellscripts/LineComparison.java
+++ src/shellscripts/LineComparison.java
@@ -3,12 +3,6 @@ package shellscripts;
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Line Comparison");
-		int x1 = 10;
-		int y1 = 10;
-		int x2 = 6;
-		int y2 = 7;
-		double lineLength = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		System.out.println(lineLength);
 		
 	}
 }
